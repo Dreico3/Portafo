79b6459cbefa348806fff9c1be4e2417e446897f
Erick <fis9.81@gmail.com>
2022-06-29 16:29:37 -0400

    logo resuelto

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from './css/navbar.module.css'
+import styles from './css/navbar.module.css';
+import logo from "../img/logoEK.png";
 export default function Navbar() {
     return (
         <div className={styles.navbar}>
-            <Link to='/' className={styles.link}>
-                <h1 className={styles.uno}>E-K-C-C</h1>
-            </Link>
-            
+            <div className={styles.uno}>
+                <Link to='/' className={styles.link}>
+                    <img src={logo} alt="aqui va el logo?" className={styles.logo} />
+                </Link>
+
+            </div>
             <div className={styles.dos}>
                 <a className={styles.link} href='#footer'>
                     <span>Contactame</span>
--- src/containers/Portafolio.tsx
+++ src/containers/Portafolio.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './css/portafolio.module.css';
-export default function Portafolio(){
-    const videos = ["https://www.youtube.com/embed/H083chy1JVM", "https://www.youtube.com/embed/9w3gy2dYN_E","https://www.youtube.com/embed/N41sQJ5SZlc"]
+import styles1 from './css/navbar.module.css';
+export default function Portafolio() {
+    const videos = ["https://www.youtube.com/embed/H083chy1JVM", "https://www.youtube.com/embed/9w3gy2dYN_E", "https://www.youtube.com/embed/N41sQJ5SZlc"]
 
     return (
-        <div className={styles.portafolio}>
+        <div >
+            <div className={styles1.navbar}>
+                <Link to='/' className={styles1.link}>
+                    <h1 className={styles1.uno}>Regresar</h1>
+                </Link>
+            </div>
+            <div className={styles.texto}>
+                <h2>aun no sabemos como mostrar los proyectos ya hechos</h2>
+                <p>
+                    este problema se solucionara cuando los proyectos esten pulidos 
+                    para ver el codigo fuente de cada uno de ellos precione <a href="https://github.com/Dreico3" target="_blank" className={styles.texto}>aqui...</a>
+                </p>
+            </div>
             {
-                videos.map((v,i)=>{
-                    return(
-                        <iframe key={i} width="560" height="315" src={v} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+               /*  videos.map((v, i) => {
+                    return (
+                        <div>
+                            <iframe key={i} width="560" height="315" src={v} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+
+                        </div>
                     )
 
-                })
+                }) */
             }
         </div>
     )
